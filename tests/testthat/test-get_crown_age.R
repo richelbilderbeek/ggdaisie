07e684134ae213aa33d6e22d632c97fb322a664d
richelbilderbeek <richel@richelbilderbeek.nl>
2018-06-11 16:54:06 +0200

    'ggd_create_phylos' creates phylos of different crown ages for now

--- tests/testthat/test-get_crown_age.R
+++ tests/testthat/test-get_crown_age.R
@@ -3,5 +3,5 @@ context("get_crown_age")
 test_that("use", {
   phylogeny <- ape::read.tree(text = "(a:15,b:15):1;")
   created <- get_crown_age(phylogeny)
-  testit::assert(created == 15)
+  expect_equal(15, created)
 })
--- tests/testthat/test-ggd_create_phylos.R
+++ tests/testthat/test-ggd_create_phylos.R
@@ -17,5 +17,6 @@ test_that("use", {
   phylos <- ggd_create_phylos(df, island_age = 4)
   expect_true(class(phylos) == "multiPhylo")
   expect_true(length(phylos) == 2)
-
+  expect_equal(0.34, get_crown_age(phylos[[1]]))
+  expect_equal(3, get_crown_age(phylos[[2]]))
 })
