ac9cec465b8d1b625adc8469209348f5ce646d84
richelbilderbeek <richel@richelbilderbeek.nl>
2018-06-11 17:07:40 +0200

    Can plot multiple immigrants

--- tests/testthat/test-ggdaisie.R
+++ tests/testthat/test-ggdaisie.R
@@ -15,7 +15,8 @@ test_that("use", {
 
   df <- get_daisie_data("Galapagos_datatable.RData")
   df <- df[ df$Status == "Non_endemic", ]
-  df <- cbind(df)
+  df <- rbind(df, df)
+  df$Branching_times[2] <- 3 # Fake
   expect_silent(
     ggdaisie(df, island_age = 4)
   )
