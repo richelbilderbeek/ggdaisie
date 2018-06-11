31e32fa12118de5a153525fff6bbdc62e33fc61f
richelbilderbeek <richel@richelbilderbeek.nl>
2018-06-11 17:59:08 +0200

    Fix 'ggd_brts_to_rnd_phylo'

--- R/ggd_brts_to_rnd_phylo.R
+++ R/ggd_brts_to_rnd_phylo.R
@@ -2,16 +2,18 @@
 ggd_brts_to_rnd_phylo <- function(
   branching_times
 ) {
+  testit::assert(length(branching_times) >= 1)
   branching_times <- sort(branching_times)
   # Shortest Branching Time
   sbt <- branching_times[1]
   init_newick <- paste0("(A:", sbt,",B:", sbt,");")
   phylo <- ape::read.tree(text = init_newick)
+  if (length(branching_times) == 1) return(phylo)
   for (i in seq(2, length(branching_times))) {
     phylo <- ribir::add_outgroup_to_phylogeny(
       phylogeny = phylo,
       stem_length = branching_times[i] - branching_times[i-1],
-      outgroup_name = LETTERS[i]
+      outgroup_name = LETTERS[i + 1]
     )
   }
   phylo
--- tests/testthat/test-ggd_brts_to_rnd_phylo.R
+++ tests/testthat/test-ggd_brts_to_rnd_phylo.R
@@ -1,13 +1,25 @@
 context("ggd_brts_to_rnd_phylo")
 
-test_that("use, 2 taxa", {
+test_that("use, 1 branching time, thus 2 taxa", {
+  phylo <- ggdaisie:::ggd_brts_to_rnd_phylo(branching_times = c(42))
+  expect_true(class(phylo) == "phylo")
+  expect_equal(ggdaisie:::get_crown_age(phylo), 42)
+  expect_equal(ape::Ntip(phylo), 2)
+  expect_silent(ape::plot.phylo(phylo))
+})
+
+test_that("use, 2 branching times, thus 3 taxa", {
   phylo <- ggdaisie:::ggd_brts_to_rnd_phylo(branching_times = c(1, 2))
   expect_true(class(phylo) == "phylo")
   expect_equal(ggdaisie:::get_crown_age(phylo), 2)
+  expect_equal(ape::Ntip(phylo), 3)
+  expect_silent(ape::plot.phylo(phylo))
 })
 
 test_that("use", {
   phylo <- ggdaisie:::ggd_brts_to_rnd_phylo(branching_times = c(1, 3, 2))
   expect_true(class(phylo) == "phylo")
   expect_equal(ggdaisie:::get_crown_age(phylo), 3)
+  expect_equal(ape::Ntip(phylo), 4)
+  expect_silent(ape::plot.phylo(phylo))
 })
