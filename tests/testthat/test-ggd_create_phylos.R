9c1196c22b8e8c915835ab9632cc97e5c4c1ac57
richelbilderbeek <richel@richelbilderbeek.nl>
2018-06-11 19:19:50 +0200

    All tests pass

--- tests/testthat/test-ggd_create_phylos.R
+++ tests/testthat/test-ggd_create_phylos.R
@@ -23,13 +23,12 @@ test_that("use Non_endemic", {
 })
 
 test_that("use Endemic", {
-  skip("WIP")
   island_age <- 4
   df <- get_daisie_data("Galapagos_datatable.RData")
   df <- df[ df$Status == "Endemic", ]
   phylos <- ggd_create_phylos(df, island_age = island_age)
   expect_true(class(phylos) == "multiPhylo")
   expect_true(length(phylos) == 2)
-  expect_equal(island_age, get_crown_age(phylos[[1]]))
-  expect_equal(island_age, get_crown_age(phylos[[2]]))
+  expect_equal(island_age, ggdaisie:::get_crown_age(phylos[[1]]))
+  expect_equal(island_age, ggdaisie:::get_crown_age(phylos[[2]]))
 })
